d503892be93e0bce0011da1cac1ded56e3ef422b
Loc M Nguyen <lokywin@gmail.com>
2016-05-10 20:37:10 -0700

    Add Babel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import uniqueRandomArray from 'unique-random-array';
 import starWarsNames from './starwars-names.json';
 
+var getRandomItem = uniqueRandomArray(starWarsNames);
+
 module.exports = {
   all: starWarsNames,
-  random: uniqueRandomArray(starWarsNames)
+  random: random
 };
+
+function random(n) {
+  if (n === undefined) {
+    return getRandomItem();
+  } else {
+    var randomItems = [];
+    for (var i = 0; i < n; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
